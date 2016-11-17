8f4d07dfc7480da53e8e18cf0c54d4c323a3819d
btoland <root@beaglebone.(none)>
2016-11-17 21:57:57 +0000

    adding servo.c

---
+++ exercises/ben/week9/servo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv)
+{
+	if(system("echo bone_pwm_P9_22 > $SLOTS"))
+	{
+		printf("P9_22 PWM Already loaded!\n");
+	}
+	else
+	{
+		printf("P9_22 PWM Loaded!\n");
+		system("echo am33xx_pwm > $SLOTS");
+	}
+
+	return 0;
+}
