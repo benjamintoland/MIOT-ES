8fd89807e7e8e44d58c05e112a26e60e49258923
marycowan <mary.cowan@itb.ie>
2017-01-06 15:27:59 +0000

    printing on lcd but not temp yet simple.cpp only

--- exercises/mary/eclipse-projects/miniproj1/simple.cpp
+++ exercises/mary/eclipse-projects/miniproj1/simple.cpp
@@ -5,7 +5,7 @@
 * directory for copyright and GNU GPLv3 license information.            */
 
 #include<iostream>
-#include<unistd.h> //for usleep
+#include<unistd.h>
 #include"GPIO.h"
 #include"PWM.h"
 #include "I2CDevice.h"
@@ -14,10 +14,14 @@
 #include<sstream>
 #include <math.h>
 #include <pthread.h>
+//#include<sys/ioctl.h>
+#include<linux/i2c-dev.h>
+//#include<fcntl.h>
+#include "I2CDevice.h"
 using namespace exploringBB;
 using namespace std;
 #define TEMP_PATH "/sys/bus/iio/devices/iio:device0/in_voltage"
-//#define SLOTS "/sys/devices/bone_capemgr.9/slots" This line can be added in case SLOTS not set up on BBB.
+#define SLOTS "/sys/devices/bone_capemgr.9/slots" //This line can be added in case SLOTS not set up on BBB.
 const int B =4275;
 int readAnalog(int number){
    stringstream ss;
@@ -30,33 +34,19 @@ int readAnalog(int number){
 }
 int callbackFunction(int var){
 	cout << "BBB Button Pressed!" << var << endl;
-	/*if(var%2 ==0)
-	{
-		system("/usr/sbin/ntpdate -b -s -u ie.pool.ntp.org");
-				system("date");
-				usleep(500000);
-				var=1;
-	}
-
-	else
-	{
-		cout << "The temp is:??? *****************" << endl;
-		var=0;
-	}
-*/
-			return var;
+	system("/usr/sbin/ntpdate -b -s -u ie.pool.ntp.org");
+		   		 	   					system("date");
+				   						return var;
 }
 
 int main(){
+	char buffer [16];
+	char buffer2 [16];
 
 	if(system("echo BB-ADC > $SLOTS"))
 	{
 		cout << "BB-ADC DTO already loaded" << endl;
 	}
-	/*if(system("echo i2c1togpio > $SLOTS"))
-		{
-			cout << "i2ctoGPIO already loaded" << endl;
-		}*/
 	if(system("echo bone_pwm_P9_22 > $SLOTS"))
 		{
 			cout << "PWM  DTO already loaded" << endl;
@@ -68,58 +58,94 @@ int main(){
 		system("echo am33xx_pwm > $SLOTS");
 		system("echo i2c1togpio > $SLOTS");
 	}
-	// I2CDevice i2cdevice("I2C2 _P9_17");//*************************check pin !!!******************
-		PWM pwm("pwm_test_P9_22.13");
-	//cout << "BeagleBone Poll Test" << endl;
+	 I2CDevice lcd(1,0x3E);//messing around to figure out
+	 I2CDevice rgb(1,0x62);
+	 lcd.open();
+	 rgb.open();
+	 lcd.writeRegister(0x80,0x01);//clear screen
+	 lcd.writeRegister(0x80,0x0F);//on with blinking
+	 lcd.writeRegister(0x80,0x28);//two line mode
+		 	 usleep (100000);
+	 lcd.writeRegister(0x80,0xC0);//second row
+	 lcd.writeRegister(0x40,102);//f
 
-		GPIO inGPIObut(5);//all I need is a dto to make i2c1 into gpio5! easy ..:)
+	 usleep(1000000);
+	 	 	 usleep(1000000);
+	 	lcd.writeRegister(0x80,0x08);//turn off
+
+	 	rgb.writeRegister(0x00,0x00);
+	 	rgb.writeRegister(0x08,0xFF);
+	 	rgb.writeRegister(0x01,0x20);
+	 	rgb.writeRegister(0x03,0x3F);
+	 		 	usleep(1000000);
+	 		 	rgb.writeRegister(0x03,0x00);
+		PWM pwm("pwm_test_P9_22.13");
+		GPIO inGPIObut(5),outGPIO(14), inGPIO(2);//gpio(2) is J6 rx (SERV0 CONTROL INPUT), gpio(14) is j2 rx (LED CONTROL OUTPUT);
+													//all I need is a dto to make i2c1 into gpio5! easy ..:)
 		//GPIO outGPIO(4);
 
 		inGPIObut.setDirection(INPUT);
 		inGPIObut.setEdgeType(RISING);
-
-		//outGPIO.setDirection(OUTPUT);
-
-		cout << "GPIO(5) has value: " << inGPIObut.getValue() << endl;
-		GPIO outGPIO(14), inGPIO(2);//gpio(2) is J6 rx (SERV0 CONTROL INPUT), gpio(14) is j2 rx (LED CONTROL OUTPUT)
 		outGPIO.setDirection(OUTPUT);
 		outGPIO.setValue(LOW);
-		 cout << "heater off:" << endl;
 
-   pwm.setPeriod(20000000);         // Set the period in ns, 20ms
+		//outGPIO.setDirection(OUTPUT); not needed
+
+		cout << "GPIO(5) has value: " << inGPIObut.getValue() << "..0 means button not pressed" << endl;
+		cout << "start with heater off:" << endl;
+
+		 pwm.setPeriod(20000000);         // Set the period in ns, 20ms
    	     pwm.setDutyCycle(5.0f);       // Set the duty 5%
    	     pwm.setPolarity(PWM::ACTIVE_HIGH);
-   	     cout << "vent closed:" << endl;// using active low PWM
+   	     cout << "start with vent closed:" << endl;// using active low PWM
    	     pwm.run();
-   cout << "Starting the readTHermistor program" << endl;
-  int repcount =0;
-   while(1)
+   	   inGPIObut.setDebounceTime(2000);
+   	   inGPIObut.waitForEdge(&callbackFunction);
+     		 cout << "Poll Started: Press the button if you like:" << endl;
+   	      while(1)
    {
-	   /*cout << "GPIO(5) has value: " << inGPIObut.getValue() << endl;
-	  inGPIObut.setDebounceTime(200);
-	 int count= (inGPIObut.waitForEdge(&callbackFunction)+1);
-	   		count+=1;
-	   	  cout << "Poll Started: Press the button:" << endl;
-	   	   usleep(10000);
-	   	   cout << "Finished sleeping for .10 seconds" << endl;*/
-	   usleep (5000);// there was a bit of bounce.
-	   int value = readAnalog(0);
-   cout << "The Thermistor value was " << value << " out of 4275." << endl;
-   float R= 4096.0/((float)value)-1.0;
+	   cout << "Starting the readTHermistor program" << endl;
+	  	  			   		usleep(5000000);
+	  	   lcd.writeRegister(0x80,0x0F);//on with blinking
+	  	   lcd.writeRegister(0x80,0x28);//two line mode
+	  	   cout << "Finished sleeping for 5 seconds" << endl;
+	   	  	   	  int value = readAnalog(0);
+   cout << "The Thermistor value was " << value << " out of 4095." << endl;
+   float R= 4095.0/((float)value)-1.0;
    R = 100000.0*R;
-   float temperature =1.0/(log(R/100000.0)/B+1/298.15)-273.15;// convert an val to temp C.
-
-     //   float R = 1023.0/((float)a)-1.0;
-      R = 100000.0*R;
-   cout << "Temerature is : " << temperature << "Degrees Celcius" << endl;
+   float temperature =1.0/(log(R/100000.0)/B+1/298.15)-263.15;// convert an val to temp C.
+          R = 100000.0*R;
+   cout << "Temperature is : " << temperature << "Degrees Celcius" << endl;
    usleep(50000);
-   if ((value >1150)&&(value <1250))
+   if ((value >1150)&&(value <1250))//set range so easy to test by putting finger on thermistor to heat up, ice cube to cool.
    {
 	   pwm.setDutyCycle(5.0f); //low duty cycle,9 o clock..
 	   outGPIO.setValue(LOW);
 	   cout << "heater still off:" << endl;
 	   cout << "vent closed:" << endl;
-   }
+	   sprintf(buffer,"heatoff,ventcls");
+	   stringstream ss (stringstream::in | stringstream::out);
+	   ss << temperature;
+	   string test = ss.str();
+	   sprintf(buffer2, test);
+	   cout << "Test value in buffer2 :" <<test << endl;
+	  //buffer2=temperature;
+	  	  //	 lcd.writeRegister(0x80,0x0F);//on with blinking
+	   //	 lcd.writeRegister(0x80,0x28);//two line mode
+	   	 lcd.writeRegister(0x80,0x80);//row 0, col 0
+	   	 int i=0;
+	   	 while(buffer[i]!='\0')
+	   	 {
+	   		lcd.writeRegister(0x40,buffer[i]);//buffer one by one
+	   		i++;
+	   	 }
+	   	lcd.writeRegister(0x80,0xC0);//row 1, col 0
+	   		   	 i=0;
+	   		   	 while(buffer2[i]!='\0')
+	   		   	 {
+	   		   		lcd.writeRegister(0x40,buffer2[i]);//buffer one by one
+	   		   		i++;}
+	 }
 
    else if (value <= 1150)
    {
@@ -134,8 +160,7 @@ int main(){
    }
 
 
-   cout << "GPIO(5) has value: " << inGPIObut.getValue() << endl;
- 	  inGPIObut.setDebounceTime(200);
+   /* 	  inGPIObut.setDebounceTime(200);
  	 int count= inGPIObut.waitForEdge(&callbackFunction);
  	   		count+=1;
  	   		repcount +=count;
@@ -153,8 +178,8 @@ int main(){
  	   		{
  	   			cout << "The temp is:??? *****************" << endl;
  	   			 	   		}
-}
-
+}*/
+   }
    return 0;
 
 }
